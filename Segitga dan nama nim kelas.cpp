0ad1d308f2ae889232fc73cc9584537b32991de9
icallmm223-design <icallmm223@gmail.com>
2025-12-02 14:57:17 +0700

    Create Segitga dan nama nim kelas.cpp

---
+++ Segitga dan nama nim kelas.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+int main () {
+    int n = 5; // jumlah baris
+        
+    for (int i = 0; i < n; i++) {
+        
+        // spasi agar bentuknya segitiga
+        for (int s = 0; s < n - i; s++) {
+            cout << " ";
+            }
+            
+        
+        int val = 1; // nilai
+        for (int j = 0; j <= i; j++) {
+            cout << val << " ";
+            
+            //rumus
+            val = val * (i - j) / (j + 1);
+        }
+        cout << endl;
+        
+    }
+    cout << "==========";
+    
+    cout << "\nname; mohamad rifky ramadani";
+    cout << "\nNIM; 23104410065";
+    cout << "\nProdi; Teknik informatika - 1B";
+
+    cout << "\n===========";
+ return 0;   
+}
